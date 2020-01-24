529813ee7d0574b2c428e0099d8d93d33bfde800
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2020-01-24 02:22:32 +0000

    Can now disable the visdom part and we can still see in the console the best iou

--- python/callbacks/state_callback.py
+++ python/callbacks/state_callback.py
@@ -5,15 +5,26 @@ class StateCallback(Callback):
     def __init__(self):
         pass
 
-    def after_forward_pass(self, phase, loss, **kwargs):
+    def after_forward_pass(self, phase, loss, pred_softmax, target, cloud, **kwargs):
         phase.iter_nr+=1
         phase.samples_processed_this_epoch+=1
         phase.loss_acum_per_epoch+=loss
 
+        phase.scores.accumulate_scores(pred_softmax, target, cloud.m_label_mngr.get_idx_unlabeled() )
+
     def epoch_started(self, phase, **kwargs):
         phase.loss_acum_per_epoch=0.0
+        phase.scores.start_fresh_eval()
 
     def epoch_ended(self, phase, **kwargs):
+        phase.scores.update_best()
+
+        #for evaluation phase print the iou
+        if not phase.grad:
+            mean_iou=phase.scores.avg_class_iou(print_per_class_iou=False)
+            best_iou=phase.scores.best_iou
+            print("iou for phase_", phase.name , " at epoch ", phase.epoch_nr , " is ", mean_iou, " best mean iou is ", best_iou)
+
         phase.epoch_nr+=1
 
     def phase_started(self, phase, **kwargs):
--- python/callbacks/visdom_callback.py
+++ python/callbacks/visdom_callback.py
@@ -11,12 +11,8 @@ class VisdomCallback(Callback):
         self.vis.log(phase.iter_nr, loss.item(), "loss_"+phase.name, "loss_"+phase.name, smooth=True)
         if phase.grad:
             self.vis.log(phase.iter_nr, lr, "lr", "lr", smooth=False)
-        # self.iter_nr+=1
 
-        phase.scores.accumulate_scores(pred_softmax, target, cloud.m_label_mngr.get_idx_unlabeled() )
 
     def epoch_ended(self, phase, **kwargs):
-        phase.scores.update_best()
         mean_iou=phase.scores.avg_class_iou(print_per_class_iou=False)
         self.vis.log(phase.epoch_nr, mean_iou, "iou_"+phase.name, "iou_"+phase.name, smooth=False)
-        phase.scores.start_fresh_eval()
