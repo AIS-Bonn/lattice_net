d53cef702efc8deaf84de948dcd0dc9ac2f174eb
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2020-01-24 01:38:15 +0000

    Restructure of code

---
+++ python/callbacks/state_callback.py
@@ -0,0 +1,23 @@
+from callbacks.callback import *
+
+class StateCallback(Callback):
+
+    def __init__(self):
+        pass
+
+    def after_forward_pass(self, phase, loss, **kwargs):
+        phase.iter_nr+=1
+        phase.samples_processed_this_epoch+=1
+        phase.loss_acum_per_epoch+=loss
+
+    def epoch_started(self, phase, **kwargs):
+        phase.loss_acum_per_epoch=0.0
+
+    def epoch_ended(self, phase, **kwargs):
+        phase.epoch_nr+=1
+
+    def phase_started(self, phase, **kwargs):
+        phase.samples_processed_this_epoch=0
+
+    def phase_ended(self, phase, **kwargs):
+        phase.loader.reset()
