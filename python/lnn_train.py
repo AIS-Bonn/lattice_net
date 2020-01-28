cece6601da8878a9d2a0ef8c429b0eed405dad3c
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2020-01-28 10:32:31 +0000

    Printing the nr of lattice vertices and the points per simplex

--- python/lnn_train.py
+++ python/lnn_train.py
@@ -22,9 +22,9 @@ from callbacks.phase import *
 from optimizers.over9000.radam import *
 
 
-config_file="lnn_train_shapenet.cfg"
+# config_file="lnn_train_shapenet.cfg"
 # config_file="lnn_train_semantic_kitti.cfg"
-# config_file="lnn_train_scannet.cfg"
+config_file="lnn_train_scannet.cfg"
 
 torch.manual_seed(0)
 
