1db67d7fdfb64cbe34ada9f59654d278c14775e0
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2020-03-23 10:08:17 +0000

    More settings to start another run on infcuda2 with weighter nll

--- python/lnn_train.py
+++ python/lnn_train.py
@@ -26,8 +26,8 @@ from optimizers.over9000.radam import *
 # from optimizers.pytorch_optimizer.torch_optimizer.adamod import *
 
 
-config_file="lnn_train_shapenet.cfg"
-# config_file="lnn_train_semantic_kitti.cfg"
+# config_file="lnn_train_shapenet.cfg"
+config_file="lnn_train_semantic_kitti.cfg"
 # config_file="lnn_train_scannet.cfg"
 
 torch.manual_seed(0)
