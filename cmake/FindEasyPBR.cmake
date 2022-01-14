e7bad5912d3a1a1672748387cd5a2b8ff87fb85e
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2022-01-14 10:39:23 +0000

    Updated cmake for easypbr

--- cmake/FindEasyPBR.cmake
+++ cmake/FindEasyPBR.cmake
@@ -24,7 +24,7 @@ message("EASYPPBR source is ", ${EASYPBR_SRC_PATH})
 # set(EASYPBR_SRC_PATH "/media/rosu/Data/phd/c_ws/src/easy_pbr")
 
 find_path(EASYPBR_CORE_INCLUDE_DIR  NAMES easy_pbr/Viewer.h PATHS "${EASYPBR_SRC_PATH}/include" )
-find_path(EASYPBR_EASYGL_INCLUDE_DIR  NAMES Texture2D.h PATHS "${EASYPBR_SRC_PATH}/deps/easy_gl" )
+find_path(EASYPBR_EASYGL_INCLUDE_DIR  NAMES easy_gl/Texture2D.h PATHS "${EASYPBR_SRC_PATH}/deps/easy_gl/include" )
 find_path(EASYPBR_LOGURU_INCLUDE_DIR  NAMES loguru.hpp PATHS "${EASYPBR_SRC_PATH}/deps/loguru" )
 find_path(EASYPBR_CONFIGURU_INCLUDE_DIR  NAMES configuru.hpp PATHS "${EASYPBR_SRC_PATH}/deps/configuru" )
 find_path(EASYPBR_UTILS_INCLUDE_DIR  NAMES Profiler.h PATHS "${EASYPBR_SRC_PATH}/deps/utils/include" )
@@ -33,11 +33,14 @@ find_path(EASYPBR_IMGUI_INCLUDE_DIR  NAMES imgui.h PATHS "${EASYPBR_SRC_PATH}/de
 find_path(EASYPBR_IMGUIZMO_INCLUDE_DIR  NAMES ImGuizmo.h PATHS "${EASYPBR_SRC_PATH}/deps/imguizmo" )
 find_path(EASYPBR_BETTERENUM_INCLUDE_DIR  NAMES enum.h PATHS "${EASYPBR_SRC_PATH}/deps/better_enums" )
 find_path(EASYPBR_EASYPYTORCH_INCLUDE_DIR  NAMES UtilsPytorch.h PATHS "${EASYPBR_SRC_PATH}/deps/easy_pytorch" )
+find_path(EASYPBR_PYBIND_INCLUDE_DIR  NAMES pybind11/pybind11.h PATHS "${EASYPBR_SRC_PATH}/deps/pybind11/include" ) #important because any module that tries to have python modules that interact with easypbr should be built with the same pybind version
+find_path(EASYPBR_TINYPLY_INCLUDE_DIR  NAMES tinyply.h PATHS "${EASYPBR_SRC_PATH}/deps/tiny_ply/source" )
 
-set( EASYPBR_INCLUDE_DIR ${EASYPBR_SRC_PATH}/extern ${EASYPBR_CORE_INCLUDE_DIR} ${EASYPBR_EASYGL_INCLUDE_DIR} ${EASYPBR_LOGURU_INCLUDE_DIR} 
-                                                    ${EASYPBR_CONFIGURU_INCLUDE_DIR} ${EASYPBR_UTILS_INCLUDE_DIR} ${EASYPBR_CONCURRENTQUEUE_INCLUDE_DIR} 
-                                                    ${EASYPBR_IMGUI_INCLUDE_DIR} ${EASYPBR_IMGUIZMO_INCLUDE_DIR} ${EASYPBR_BETTERENUM_INCLUDE_DIR} 
-                                                    ${EASYPBR_EASYPYTORCH_INCLUDE_DIR} )
+set( EASYPBR_INCLUDE_DIR ${EASYPBR_SRC_PATH}/extern ${EASYPBR_CORE_INCLUDE_DIR} ${EASYPBR_EASYGL_INCLUDE_DIR} ${EASYPBR_LOGURU_INCLUDE_DIR}
+                                                    ${EASYPBR_CONFIGURU_INCLUDE_DIR} ${EASYPBR_UTILS_INCLUDE_DIR} ${EASYPBR_CONCURRENTQUEUE_INCLUDE_DIR}
+                                                    ${EASYPBR_IMGUI_INCLUDE_DIR} ${EASYPBR_IMGUIZMO_INCLUDE_DIR} ${EASYPBR_BETTERENUM_INCLUDE_DIR}
+                                                    ${EASYPBR_EASYPYTORCH_INCLUDE_DIR}  ${EASYPBR_PYBIND_INCLUDE_DIR}  ${EASYPBR_TINYPLY_INCLUDE_DIR}
+                                                    )
 
 # message("--------------------EASYPPBR include dir is at ", ${EASYPBR_INCLUDE_DIR})
 
@@ -50,5 +53,3 @@ find_library(EASYPBR_LIBRARY
 # message("--------------------EASYPPBR lib dir is at ", ${EASYPBR_LIBRARY})
 
 add_definitions( -DDEFAULT_CONFIG="${EASYPBR_SRC_PATH}/config/default_params.cfg" )
-
-
